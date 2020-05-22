186d3b9153e1d835f97b69b7551d2ce3d99fd226
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-05-22 16:44:44 +0200

    css modules added

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from './App.modules.css';
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -61,19 +61,7 @@ class App extends Component {
   };
 
   render() {
-    // const style = {
-    //   backgroundColor: "green",
-    //   color: 'white',
-    //   font: "inherit",
-    //   border: "1px solid blue",
-    //   padding: "8px",
-    //   cursor: "pointer", 
-    //   ':hover':  { 
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
-
+    let btnClass = '';
     let persons = null;
 
     if(this.state.showPersons){
@@ -92,26 +80,24 @@ class App extends Component {
              })}
           </div> 
       );
-
-
+      btnClass = classes.Red
     }
-
     // Join the strings together, you'll get "red bold"
     // let classes = ['red', 'bold'].join(' '); 
-    let classes = []; 
+    let assignedClasses = []; 
     if(this.state.persons.length <= 2) { 
-      classes.push('red'); //classes = ['red']
+      assignedClasses.push('red'); //classes = ['red']
     }
     if(this.state.persons.length <= 1) { 
-     classes.push('bold'); // classes = ['red', 'bold']
+     assignedClasses.push('bold'); // classes = ['red', 'bold']
     }
 
     return (
    
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}> This is really working!</p>
-        <button className="button" onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(' ')}> This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
         {persons}
