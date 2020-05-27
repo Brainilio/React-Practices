dafe848e58c97c76348e00f74ae1f3f4373f1a6c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-05-27 12:53:22 +0200

    uinsg contexttype to access contextapi

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -13,9 +13,12 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  // Give access to context even in componentdidmount
+  static contextType = AuthContext; 
 
   componentDidMount() { 
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated)
   }
 
 
@@ -23,11 +26,8 @@ class Person extends Component {
     console.log("Person.js rendering");
     return ( 
         <Fragment> 
-          <AuthContext.Consumer>
-            {(context) => 
-              context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-            } 
-          </AuthContext.Consumer>
+              {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+           
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
