de405498f0b203485451be37e454d7443fb5f2c6
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-21 19:40:14 +0200

    REDUCER CALLBACK

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -4,9 +4,10 @@ import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import { createStore } from "redux"
+import reducer from "./store/reducer"
 
-//create redux store
-const store = createStore()
+//create redux store and callback the reducer.
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
