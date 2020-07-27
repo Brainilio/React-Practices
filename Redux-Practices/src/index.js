7c8c6a3056201f0e058af42241a76b264088f9f0
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-27 12:12:36 +0200

    log middleware

--- Redux-Practices/src/index.js
+++ Redux-Practices/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
-import { createStore, combineReducers } from "redux"
+import { createStore, combineReducers, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import counterReducer from "./store/reducers/counter"
 import resultReducer from "./store/reducers/result"
@@ -14,8 +14,21 @@ const rootReducer = combineReducers({
 	res: resultReducer,
 })
 
-//create redux store and callback the reducer.
-const store = createStore(rootReducer)
+//log store and middleware
+const logger = (store) => {
+	return (next) => {
+		return (action) => {
+			console.log("[MIDDLEWARE] Dispatching", action)
+			//let action pass to reducer
+			const result = next(action)
+			console.log("[MIDDLEWARE] next state", store.getState())
+			return result
+		}
+	}
+}
+
+//create redux store and callback the reducer + middleware
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(
 	<Provider store={store}>
