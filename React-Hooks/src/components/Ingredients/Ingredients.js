89b5880bfe8e9415b55ee11a57f820bfe7d3581f
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-05 13:40:08 +0200

    deleted ingredeni

--- React-Hooks/src/components/Ingredients/Ingredients.js
+++ React-Hooks/src/components/Ingredients/Ingredients.js
@@ -5,9 +5,30 @@ import IngredientList from "./IngredientList"
 import Search from "./Search"
 
 const Ingredients = (props) => {
-	const [ingredients, setIngredients] = React.useState([])
+	const [ingredients, setIngredients] = React.useState([
+		{
+			id: 3,
+			title: "Banana",
+			amount: 3,
+		},
+		{
+			id: 2,
+			title: "Apples",
+			amount: 3,
+		},
+		{
+			id: 5,
+			title: "Oranges",
+			amount: 5,
+		},
+	])
 
 	const addIngredientHandler = (ingredient) => {
+		fetch("https://dummyproject-35081.firebaseio.com/ingredients.json", {
+			method: "POST",
+			body: JSON.stringify(ingredient),
+		})
+
 		setIngredients((prevIngredients) => [
 			...prevIngredients,
 			{
@@ -16,13 +37,28 @@ const Ingredients = (props) => {
 			},
 		])
 	}
+
+	const removeIngredientHandler = (id) => {
+		let oldIngredients = [...ingredients]
+		oldIngredients.map((ig) => {
+			if (ig.id === id) {
+				let ingredientIndex = oldIngredients.indexOf(ig)
+				return oldIngredients.splice(ingredientIndex, 1)
+			}
+		})
+		setIngredients([...oldIngredients])
+	}
+
 	return (
 		<div className="App">
 			<IngredientForm onAdd={addIngredientHandler} />
 
 			<section>
 				<Search />
-				<IngredientList onRemoveItem={() => {}} ingredients={ingredients} />
+				<IngredientList
+					onRemoveItem={removeIngredientHandler}
+					ingredients={ingredients}
+				/>
 			</section>
 		</div>
 	)
