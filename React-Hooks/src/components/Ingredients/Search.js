3b63a96b0979b9073415c1840a04c06744e6f040
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-05 14:31:55 +0200

    set time out for closure

--- React-Hooks/src/components/Ingredients/Search.js
+++ React-Hooks/src/components/Ingredients/Search.js
@@ -7,29 +7,34 @@ const Search = React.memo((props) => {
 	//destructure props
 	const { onLoadIngredients } = props
 	const [enteredFilter, setEnteredFilter] = React.useState("")
-
+	const inputRef = React.useRef()
 	// only fires when enteredfilter changes.
 	React.useEffect(() => {
-		//query string
-		const query =
-			enteredFilter.length === 0
-				? ""
-				: `?orderBy="title"&equalTo="${enteredFilter}"`
-
-		fetch("https://dummyproject-35081.firebaseio.com/ingredients.json" + query)
-			.then((response) => response.json())
-			.then((data) => {
-				const loadedIngredients = []
-				for (const key in data) {
-					loadedIngredients.push({
-						id: key,
-						title: data[key].title,
-						amount: data[key].amount,
+		setTimeout(() => {
+			if (enteredFilter === inputRef.current.value) {
+				//query string
+				const query =
+					enteredFilter.length === 0
+						? ""
+						: `?orderBy="title"&equalTo="${enteredFilter}"`
+				fetch(
+					"https://dummyproject-35081.firebaseio.com/ingredients.json" + query
+				)
+					.then((response) => response.json())
+					.then((data) => {
+						const loadedIngredients = []
+						for (const key in data) {
+							loadedIngredients.push({
+								id: key,
+								title: data[key].title,
+								amount: data[key].amount,
+							})
+						}
+						onLoadIngredients(loadedIngredients)
 					})
-				}
-				onLoadIngredients(loadedIngredients)
-			})
-	}, [enteredFilter, onLoadIngredients])
+			}
+		}, 500)
+	}, [enteredFilter, onLoadIngredients, inputRef])
 
 	return (
 		<section className="search">
@@ -37,6 +42,7 @@ const Search = React.memo((props) => {
 				<div className="search-input">
 					<label>Filter by Title</label>
 					<input
+						ref={inputRef}
 						type="text"
 						onChange={(e) => setEnteredFilter(e.target.value)}
 					/>
