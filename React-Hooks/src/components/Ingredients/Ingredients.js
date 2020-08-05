9cf3b3f6587ccea537dc2b738e3347b3d6200890
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-05 14:49:56 +0200

    error modal & loader

--- React-Hooks/src/components/Ingredients/Ingredients.js
+++ React-Hooks/src/components/Ingredients/Ingredients.js
@@ -2,10 +2,13 @@ import React from "react"
 
 import IngredientForm from "./IngredientForm"
 import IngredientList from "./IngredientList"
+import ErrorModal from "../UI/ErrorModal"
 import Search from "./Search"
 
 const Ingredients = (props) => {
 	const [ingredients, setIngredients] = React.useState([])
+	const [isLoading, setIsloading] = React.useState(false)
+	const [error, setError] = React.useState()
 
 	React.useEffect(() => {
 		console.log("Rendering", ingredients)
@@ -16,12 +19,14 @@ const Ingredients = (props) => {
 	}, [])
 
 	const addIngredientHandler = (ingredient) => {
+		setIsloading(true)
 		fetch("https://dummyproject-35081.firebaseio.com/ingredients.json", {
 			method: "POST",
 			body: JSON.stringify(ingredient),
 			headers: { "Content-Type": "application/json" },
 		})
 			.then((response) => {
+				setIsloading(false)
 				return response.json()
 			})
 			.then((resData) => {
@@ -36,17 +41,33 @@ const Ingredients = (props) => {
 	}
 
 	const removeIngredientHandler = (id) => {
+		setIsloading(true)
 		fetch(`https://dummyproject-35081.firebaseio.com/ingredients/${id}.json`, {
 			method: "DELETE",
-		}).then((response) => {
-			setIngredients((prevIg) => prevIg.filter((ig) => ig.id !== id))
 		})
+			.then((response) => {
+				setIsloading(false)
+				setIngredients((prevIg) => prevIg.filter((ig) => ig.id !== id))
+			})
+			.catch((error) => {
+				setError("Something went wrong!")
+			})
 	}
 
 	return (
 		<div className="App">
-			<IngredientForm onAdd={addIngredientHandler} />
+			{error && (
+				<ErrorModal
+					onClose={() => {
+						setError(null)
+						setIsloading(false)
+					}}
+				>
+					{error}
+				</ErrorModal>
+			)}
 
+			<IngredientForm onAdd={addIngredientHandler} loading={isLoading} />
 			<section>
 				<Search onLoadIngredients={filteredIngredientsHandler} />
 				<IngredientList
