f83a40eda4dc43d729166c8b6d67b65b024d6bdc
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-05 12:36:54 +0200

    usestate

--- React-Hooks/src/components/Ingredients/IngredientForm.js
+++ React-Hooks/src/components/Ingredients/IngredientForm.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useState } from "react"
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card"
+import "./IngredientForm.css"
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
-    event.preventDefault();
-    // ...
-  };
+const IngredientForm = React.memo((props) => {
+	const [form, setForm] = useState({
+		name: "",
+		amount: "",
+	})
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
-});
+	const submitHandler = (event) => {
+		event.preventDefault()
+		// ...
+	}
 
-export default IngredientForm;
+	return (
+		<section className="ingredient-form">
+			<Card>
+				<form onSubmit={submitHandler}>
+					<div className="form-control">
+						<label htmlFor="title">Name</label>
+						<input
+							type="text"
+							id="title"
+							value={form.name}
+							onChange={(e) => setForm({ ...form, name: e.target.value })}
+						/>
+					</div>
+					<div className="form-control">
+						<label htmlFor="amount">Amount</label>
+						<input
+							type="number"
+							id="amount"
+							value={form.amount}
+							onChange={(e) => setForm({ ...form, amount: e.target.value })}
+						/>
+					</div>
+					<div className="ingredient-form__actions">
+						<button type="submit">Add Ingredient</button>
+					</div>
+				</form>
+			</Card>
+		</section>
+	)
+})
+
+export default IngredientForm
